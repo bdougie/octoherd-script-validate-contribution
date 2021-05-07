ea05e0c5a70e3667045548baeaecea9a4a67abb1
Brian Douglas <bdougie@users.noreply.github.com>
2021-05-07 05:28:24 +0000

    try something

--- script.js
+++ script.js
@@ -6,4 +6,13 @@
  * @param {import('@octoherd/cli').Octokit} octokit
  * @param {import('@octoherd/cli').Repository} repository
  */
-export async function script(octokit, repository) {}
+export async function script(octokit, repository) {
+  const [repoOwner, repoName] = repository.full_name.split("/");
+  octokit.log.info(`${repoOwner} label exists: ${exists}`)
+
+  const data = await octokit.request(
+    `GET /repos/${repoOwner}/${repoName}/issues/`
+  )
+
+  octokit.log.info(data)
+}
