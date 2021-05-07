ff22bca09cbcaa65c89eb7ca5539dd36a2b6df2b
brian <ilikerobot@gmail.com>
2021-05-06 21:51:02 -0700

    feat: initial version

---
+++ script.js
@@ -0,0 +1,9 @@
+// @ts-check
+
+/**
+ * octoherd-script-good-pr
+ *
+ * @param {import('@octoherd/cli').Octokit} octokit
+ * @param {import('@octoherd/cli').Repository} repository
+ */
+export async function script(octokit, repository) {}
